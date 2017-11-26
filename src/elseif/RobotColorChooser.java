c4cf2b1b5e18f588a2f1265317cce1b910acf622
Chumister <league@WTS21.attlocal.net>
2017-11-26 15:57:33 -0800

    HighLowGame, RobotColorChooser, DizzyRobot

--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -5,10 +5,14 @@
 package elseif;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
+		Robot rob = new Robot();
 
 		//3. ask the user what color they would like the robot to draw
 		
@@ -21,7 +25,30 @@ public class RobotColorChooser {
 		//2. set the pen width to 10
 		
 	    //1. make the robot draw a shape (this will take more than one line of code)
-
-
+		 rob.penDown();
+		 rob.setPenWidth(5);
+		 rob.setSpeed(10);
+		 rob.setY(290);
+		 rob.setX(250);
+		 rob.setAngle(35);
+		 rob.move(150);	
+		 rob.setAngle(-145);
+		 rob.move(75);
+		 rob.setAngle(-45);
+		 rob.move(75);
+		 rob.setAngle(135);
+		 rob.penUp();
+		 rob.move(100);
+		 rob.setAngle(90);
+		 rob.move(80);
+		 rob.setAngle(0);
+		 rob.penDown();
+		 rob.move(100);
+		 rob.setAngle(90);
+		 rob.move(50);
+		 rob.setAngle(180);
+		 rob.move(100);
+		 rob.setAngle(270);
+		 rob.setAngle(50);
 	}
 }
