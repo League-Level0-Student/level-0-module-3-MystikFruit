2098c8e5cfebcaf6d7c8f5dce03b5142980b44da
dalidog <league@WTS21.attlocal.net>
2017-12-03 16:01:04 -0800

    DizzyRobot, RobotColorChooser

--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -13,42 +13,87 @@ import java.awt.Color;
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		Robot rob = new Robot();
+		int r = 21;
+		int g = 59;
+		int b = 94;
 
-		//3. ask the user what color they would like the robot to draw
-		
-		//4. use an if/else statement to set the pen color that the user requested
+		for (int i = 0; i < 100000; i++) {
+			String color = JOptionPane.showInputDialog("What color do you want the robot to draw in?");
 
-        //5. if the user doesn’t enter anything, choose a random color
+			if (color.equalsIgnoreCase("gold")) {
+				rob.setPenColor(242, 192, 55);
+			} else if (color.equalsIgnoreCase("blue")) {
+				rob.setPenColor(51, 146, 234);
+			} else if (color.equals("gradient")) {
+				rob.setPenColor(r, g, b);
+			} else if (color.equalsIgnoreCase("purple")) {
+				rob.setPenColor(137, 73, 183);
+			} else if (color.equalsIgnoreCase("red")) {
+				rob.setPenColor(216, 52, 52);
+			} else if (color.equalsIgnoreCase("yellow")) {
+				rob.setPenColor(255, 233, 0);
+			} else if (color.equalsIgnoreCase("pink")) {
+				rob.setPenColor(255, 71, 245);
+			} else if (color.equalsIgnoreCase("orange")) {
+				rob.setPenColor(252, 154, 35);
+			} else if (color.equalsIgnoreCase("green")) {
+				rob.setPenColor(96, 242, 142);
+			} else {
+				rob.setRandomPenColor();
+			}
+			rob.penDown();
+			rob.setPenWidth(10);
+			rob.setSpeed(10);
+			rob.setY(200);
+			rob.setX(275);
+			rob.move(60);
+			rob.setAngle(90);
+			if (color.equalsIgnoreCase("gradient")) {
+				for (int j = 0; i < 250; i++) {
+					rob.move(1);
+					r++;
+					g++;
+					b++;
+					rob.setPenColor(r, g, b);
+				}
+			} else
+				rob.move(250);
 
-        //6. put a loop around your code so that you keep asking the user for more colors & drawing them
-		
-		//2. set the pen width to 10
-		
-	    //1. make the robot draw a shape (this will take more than one line of code)
-		 rob.penDown();
-		 rob.setPenWidth(5);
-		 rob.setSpeed(10);
-		 rob.setY(290);
-		 rob.setX(250);
-		 rob.setAngle(35);
-		 rob.move(150);	
-		 rob.setAngle(-145);
-		 rob.move(75);
-		 rob.setAngle(-45);
-		 rob.move(75);
-		 rob.setAngle(135);
-		 rob.penUp();
-		 rob.move(100);
-		 rob.setAngle(90);
-		 rob.move(80);
-		 rob.setAngle(0);
-		 rob.penDown();
-		 rob.move(100);
-		 rob.setAngle(90);
-		 rob.move(50);
-		 rob.setAngle(180);
-		 rob.move(100);
-		 rob.setAngle(270);
-		 rob.setAngle(50);
+			rob.setAngle(180);
+			if (color.equalsIgnoreCase("gradient")) {
+				for (int j = 0; i < 150; i++) {
+					rob.move(1);
+					r++;
+					g++;
+					b++;
+					rob.setPenColor(r, g, b);
+				}
+			} else
+				rob.move(150);
+
+			rob.setAngle(270);
+			if (color.equalsIgnoreCase("gradient")) {
+				for (int j = 0; i < 250; i++) {
+					rob.move(1);
+					r++;
+					g++;
+					b++;
+					rob.setPenColor(r, g, b);
+				}
+			} else
+				rob.move(250);
+			rob.setAngle(360);
+			if (color.equalsIgnoreCase("gradient")) {
+				for (int j = 0; i < 90; i++) {
+					rob.move(1);
+					r++;
+					g++;
+					b++;
+					rob.setPenColor(r, g, b);
+				}
+			} else
+				rob.move(90);
+
+		}
 	}
 }
--- src/extras/DizzyRobot.java
+++ src/extras/DizzyRobot.java
@@ -3,22 +3,21 @@
 
 package extras;
 
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class DizzyRobot {
 	static Robot robot = new Robot();
 	public static void main(String[] args) {
 		
-		 // 2. Ask the user how dizzy you want the robot from 1-10, then spin that number of times.
-
- // 1. Use the dance method to make the robot spin.
-	
-	
-
+		String kjlh= JOptionPane.showInputDialog("How dizzy do you want the robot to be from 1-10?");
+		int num=Integer.parseInt(kjlh);
+dance(num);	
 	}
-
 	static void dance(int numberOfSpins) {
 		for (int i = 0; i < numberOfSpins; i++) {
+			robot.setSpeed(10);
 			robot.turn(360);
 		}
 	}
